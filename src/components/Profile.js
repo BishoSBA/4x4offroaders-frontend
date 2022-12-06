51d618054010204210314209883a2b58b42aa80a
Beshir Saeed <Beshir-saeed@live.com>
2022-12-06 23:16:34 +0300

    Add post

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -80,6 +80,54 @@ const Profile = ({ profile }) => {
 								Caption
 							</label>
 						</div>
+						<div className="relative z-0 mb-6 w-full group">
+							<input
+								type="text"
+								name="vehicle"
+								id="vehicle"
+								className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer "
+								placeholder=" "
+								required
+							/>
+							<label
+								htmlFor="vehicle"
+								className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-lg"
+							>
+								Vehicle
+							</label>
+						</div>
+						<div className="relative z-0 mb-6 w-full group">
+							<input
+								type="text"
+								name="brand"
+								id="brand"
+								className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer "
+								placeholder=" "
+								required
+							/>
+							<label
+								htmlFor="brand"
+								className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-lg"
+							>
+								Brand
+							</label>
+						</div>
+						<div className="relative z-0 mb-6 w-full group">
+							<input
+								type="text"
+								name="workshop"
+								id="workshop"
+								className="block py-2.5 px-0 w-full text-lg text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer "
+								placeholder=" "
+								required
+							/>
+							<label
+								htmlFor="workshop"
+								className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-lg"
+							>
+								Workshop
+							</label>
+						</div>
 
 						<div className="mb-3">
 							<label
